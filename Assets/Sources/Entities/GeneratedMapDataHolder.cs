01b11ce3ceb84aa2ae1c494ada4d3dc8226bd1b0
Potrunks <potrunks@hotmail.com>
2023-03-09 21:24:03 +0100

    Renommage MapGeneratorDto par GeneratedMapDataHolder #34

---
+++ Assets/Sources/Entities/GeneratedMapDataHolder.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Sources.Referentiel.Enum;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Sources.Entities
+{
+    public class GeneratedMapDataHolder
+    {
+        public GeneratedMapDataHolder(GameObject firstChunckRoadInstantiated, RoadLevel firstChunckRoadLevel)
+        {
+            ChunckRoadsInstantiated = new List<GameObject> { firstChunckRoadInstantiated };
+            LastChunckRoadInstantiated = firstChunckRoadInstantiated;
+            CurrentLevel = firstChunckRoadLevel;
+            LevelBeforeTunnel = firstChunckRoadLevel;
+        }
+
+        public List<GameObject> ChunckRoadsInstantiated { get; set; }
+        public GameObject LastChunckRoadInstantiated { get; set; }
+        public RoadLevel CurrentLevel { get; set; }
+        public RoadLevel LevelBeforeTunnel { get; set; }
+    }
+}
